f6c9ae64d065eb3b17b24b576228a79aa3561579
Alex Kobilianskiy <alex_kobilianskiy@bigmir.net>
2018-02-12 15:44:29 +0200

    services and fallery complete

--- app/js/common.js
+++ app/js/common.js
@@ -6,4 +6,25 @@ $(function() {
         return false;
     });
 
+    $(".service-title").equalHeights();
+    $(".popup-gallery>img").equalHeights();
+
+
+    $(".popup-gallery").each(function(e){
+        $(this).attr("href", "#popup-gallery-"+ e)
+            .find(".window-sert").attr("id", "popup-gallery-"+e);
+    });
+
+    $('.popup-gallery').magnificPopup({
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-sert',
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        gallery: {
+            enabled: true
+        },
+
+    });
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([ // берем все библиотеки
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/equalheights/jquery.equalheights.min.js',
+		'app/libs/magnificPopup/jquery.magnific-popup.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js')) // минимизируем и соединяем в один файл
